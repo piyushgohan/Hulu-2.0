8961fc8804182af9944c995f350da5b97fd9dda7
piyush <piyushgohan@gmail.com>
2021-06-22 18:41:03 +0530

    sixth commit

--- components/Results.js
+++ components/Results.js
@@ -1,13 +1,12 @@
 import Thumbnail from "./Thumbnail"
-import FlipMove from "react-flip-move"
 
 function Results({results}) {
     return (
-        <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:flex flex-wrap justify-center">
+        <div className="px-5 my-10 sm:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:flex flex-wrap justify-center">
             {results.map(result => 
                 <Thumbnail key={result.id} result={result} />    
             )}
-        </FlipMove>
+        </div>
     )
 }
 
--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,12 +1,11 @@
 import Image from "next/image"
 import {ThumbUpIcon} from '@heroicons/react/outline';
-import {forwardRef} from 'react';
 
-const Thumbnail = forwardRef(({result},ref) => {
-
+function Thumbnail({result}){
+    
     const BASE_URL = "https://image.tmdb.org/t/p/original/";
     return (
-        <div ref={ref} className=" p-2 group cursor-pointer transistion-all duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+        <div className=" p-2 group cursor-pointer transistion-all duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
             <Image
                 layout="responsive"
                 src ={
@@ -27,6 +26,6 @@ const Thumbnail = forwardRef(({result},ref) => {
             </div>
         </div>
     )
-})
+};
 
 export default Thumbnail
